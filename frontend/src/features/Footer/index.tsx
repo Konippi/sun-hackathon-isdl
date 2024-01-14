efaffa4d6f00a459742e3e6ebd42227c828469eb
Konippi <konippi-114@outlook.jp>
2024-01-14 22:16:31 +0900

    fix: footer

--- frontend/src/features/Footer/index.tsx
+++ frontend/src/features/Footer/index.tsx
@@ -3,7 +3,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <footer className="footer">
-            &copy; 2024 Intelligent Design Systems Laboratory. All Rights Reserved.
+            &copy; 2024 Intelligent Design Systems Laboratory. <br/>All Rights Reserved.
         </footer>
     )
 }
