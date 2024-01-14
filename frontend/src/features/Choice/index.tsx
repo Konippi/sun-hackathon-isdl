36ed7686cc38660235dfd3bcf2e5c3546eaddfba
Nao <7ao.0716@gmail.com>
2024-01-14 17:48:04 +0900

    feat: choice受け渡し

--- frontend/src/features/Choice/index.tsx
+++ frontend/src/features/Choice/index.tsx
@@ -1,23 +1,24 @@
 import "./Choice.css"
-import React, {useState} from 'react';
+//import React, {useState} from 'react';
 import { Button, Text, VStack } from '@chakra-ui/react';
 
 type QuestionChoiceProps = {
   choices: string[];
+  onChoiceSelect: (choice: string) => void;
 };
 
-const QuestionChoice: React.FC<QuestionChoiceProps> = ({ choices }) => {
-    const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
+const QuestionChoice: React.FC<QuestionChoiceProps> = ({ choices, onChoiceSelect }) => {
+    // const [selectedChoice, setSelectedChoice] = useState<string | null>(null);
 
-    const handleChoiceClick = (choice: string) => {
-        // 選択された選択肢を保持する処理
-        setSelectedChoice(choice);
-        console.log(choice)
-    };
+    // const handleChoiceClick = (choice: string) => {
+    //     // 選択された選択肢を保持する処理
+    //     setSelectedChoice(choice);
+    //     console.log(choice)
+    // };
     return(
         <VStack spacing={4} align="center" pt={5}>
             {choices.map((choice, index) => (
-                <Button key={index} colorScheme="black" variant="outline" w="80%" borderRadius={30} onClick={() => handleChoiceClick(choice)}>
+                <Button key={index} colorScheme="black" variant="outline" w="80%" borderRadius={30} onClick={() => onChoiceSelect(choice)}>
                     <Text w={250} textAlign={"start"}>{choice}</Text>
                 </Button>
             ))}
